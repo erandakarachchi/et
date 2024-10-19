dacc343bb2d2f88e54a36d47b7c7e36527445c41
Eranda K <emkarachchi@gmail.com>
2024-10-19 09:48:52 +0530

    hide loading on whitelisted pages

--- et-frontend/lib/providers/APIProvider.tsx
+++ et-frontend/lib/providers/APIProvider.tsx
@@ -2,6 +2,7 @@
 import { useSession } from "@clerk/nextjs";
 import { useEffect, createContext, useContext, useState } from "react";
 import { APIClient } from "../api/client";
+import { usePathname } from "next/navigation";
 
 type Props = {
   children: React.ReactNode;
@@ -21,6 +22,10 @@ const APIProvider = ({ children }: Props) => {
   const { session } = useSession();
   const [apiClient, setApiClient] = useState<APIClient | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const pathname = usePathname();
+
+  //   Loading screen won't show on home page, sign-in page and sign-up page
+  const whitelistedPages = ["/", "/sign-in", "/sign-up"];
 
   useEffect(() => {
     const initializeApiClient = async () => {
@@ -42,7 +47,7 @@ const APIProvider = ({ children }: Props) => {
 
   return (
     <APIContext.Provider value={apiClient}>
-      {isLoading ? (
+      {isLoading && !whitelistedPages.includes(pathname) ? (
         <div className="h-screen flex items-center justify-center">
           <h1 className="text-2xl font-bold">We're getting things ready for you...</h1>
         </div>
